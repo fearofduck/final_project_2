97fbf7285229fc4cfdb83aa7e5e1483f20013a9d
Raghu Betina <raghu@firstdraft.com>
2018-03-10 22:13:42 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "ds40_seas#index"
   # Routes for the Comment resource:
   # CREATE
