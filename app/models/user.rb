f7a8d843e9ff8bba2a28863a168741c4584b0204
Raghu Betina <raghu@firstdraft.com>
2018-03-10 22:13:27 +0000

    Direct association between users and seasonalities

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :seasonalities,
+             :dependent => :destroy
+
   has_many   :ds40_seas,
              :dependent => :destroy
 
