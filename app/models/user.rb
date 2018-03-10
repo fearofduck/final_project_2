9d8a92a542d2603643c269bf4c9d9169d6a9e582
Raghu Betina <raghu@firstdraft.com>
2018-03-10 22:13:27 +0000

    Direct association between users and ds53_cts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :ds53_cts,
+             :dependent => :destroy
+
   has_many   :ds40_cts,
              :dependent => :destroy
 
