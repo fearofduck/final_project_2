b4b89006c8026c42ffb3d7e9b0ab40d627eb7861
Raghu Betina <raghu@firstdraft.com>
2018-03-10 22:13:27 +0000

    Direct association between users and forecasts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :forecasts,
+             :dependent => :destroy
+
   has_many   :cycle_times,
              :dependent => :destroy
 
