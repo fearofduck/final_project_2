89ed2d4032afb574087d48afeed7edefdbc6a0cb
Raghu Betina <raghu@firstdraft.com>
2018-03-10 22:13:27 +0000

    Direct association between ds40_seas and seasonalities

--- app/models/ds40_sea.rb
+++ app/models/ds40_sea.rb
@@ -1,6 +1,10 @@
 class Ds40Sea < ApplicationRecord
   # Direct associations
 
+  has_many   :seasonalities,
+             :foreign_key => "ds40_seas_id",
+             :dependent => :destroy
+
   belongs_to :user
 
   # Indirect associations
--- app/models/seasonality.rb
+++ app/models/seasonality.rb
@@ -1,6 +1,9 @@
 class Seasonality < ApplicationRecord
   # Direct associations
 
+  belongs_to :ds40_seas,
+             :class_name => "Ds40Sea"
+
   belongs_to :user
 
   # Indirect associations
